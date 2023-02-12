06a7202cc39cb0669eab592a28ceb7eecfbda71b
Bhupesh Shrestha <bhupesh.stha@gmail.com>
2023-02-12 18:13:28 -0500

    Layout Design Draft

---
+++ app/src/main/java/com/group1/CheckoutForm.kt
@@ -0,0 +1,11 @@
+package com.group1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_form)
+    }
+}
