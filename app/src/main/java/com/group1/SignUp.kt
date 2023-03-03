19addf375d341405f6b57f5a58c3f107b6a3f1e5
malenacaroline <malenacaroline01@gmail.com>
2023-03-03 16:47:18 -0500

    create sign up and sign in screens

---
+++ app/src/main/java/com/group1/SignUp.kt
@@ -0,0 +1,11 @@
+package com.group1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
